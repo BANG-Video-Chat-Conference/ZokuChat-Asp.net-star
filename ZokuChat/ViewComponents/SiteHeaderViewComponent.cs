f2d7ce576e01f9d83fc7589e1a30bd35d267ba80
chiposur <osurov314@gmail.com>
2018-07-31 11:00:09 -0700

    Cutting over app to use Razor Pages

---
+++ ZokuChat/ViewComponents/SiteHeaderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ZokuChat.Data;
+
+namespace ViewComponents
+{
+	public class SiteHeaderViewComponent : ViewComponent
+	{
+		private readonly ApplicationDbContext db;
+
+		public SiteHeaderViewComponent(ApplicationDbContext context)
+		{
+			db = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			// This will need to use await keyword when we retrieve items from db
+			return View();
+		}
+	}
+}
