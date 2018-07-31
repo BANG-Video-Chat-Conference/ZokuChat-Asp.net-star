0f114a5da9710d838ed13fd2eec52fd7a56cfc8c
chiposur <osurov314@gmail.com>
2018-07-30 18:34:18 -0700

    Upgrading version of dotnet core app and adding siteheader view component

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Data;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.Configuration;
