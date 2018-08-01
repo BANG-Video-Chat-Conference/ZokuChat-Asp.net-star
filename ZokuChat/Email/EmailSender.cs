cf95c18dc9b59f5016615101e5327220a883cd97
chiposur <osurov314@gmail.com>
2018-08-01 12:44:37 -0700

    Refactored email service

--- ZokuChat/Email/EmailSender.cs
+++ ZokuChat/Email/EmailSender.cs
@@ -14,7 +14,7 @@ namespace WebPWrecover.Services
 			Options = optionsAccessor.Value;
 		}
 
-		public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+		public AuthMessageSenderOptions Options { get; }
 
 		public Task SendEmailAsync(string email, string subject, string message)
 		{
@@ -23,23 +23,24 @@ namespace WebPWrecover.Services
 
 		public Task Execute(string apiKey, string subject, string message, string email)
 		{
+			// Construct email
 			var client = new SendGridClient(apiKey);
 			var msg = new SendGridMessage()
 			{
-				From = new EmailAddress("Joe@contoso.com", "Joe Smith"),
+				From = new EmailAddress("ZokuBot@ZokuChat.com", "Zoku Bot"),
 				Subject = subject,
 				PlainTextContent = message,
 				HtmlContent = message
 			};
 			msg.AddTo(new EmailAddress(email));
 
-			// Disable click tracking.
-			// See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+			// Disable click tracking
 			msg.TrackingSettings = new TrackingSettings
 			{
 				ClickTracking = new ClickTracking { Enable = false }
 			};
 
+			// Send asynch
 			return client.SendEmailAsync(msg);
 		}
 	}
