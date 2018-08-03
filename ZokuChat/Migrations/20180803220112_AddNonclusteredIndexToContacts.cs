15fba8bd45f7c07fd8c5b8c1207e5369c2c6ad9c
chiposur <osurov314@gmail.com>
2018-08-03 15:11:49 -0700

    Adding nonclustered index to Contacts

---
+++ ZokuChat/Migrations/20180803220112_AddNonclusteredIndexToContacts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZokuChat.Migrations
+{
+    public partial class AddNonclusteredIndexToContacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.CreateIndex("IX_Contacts_AspNetUsers_UserUID_ContactUID", "Contacts", new string[] { "UserUID", "ContactUID" }, unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.DropIndex("IX_Contacts_AspNetUsers_UserUID_ContactUID", "Contacts");
+        }
+    }
+}
