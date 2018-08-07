ec4ec81917c23d6d6f3ed9c4a80ad21ff8a96249
chiposur <osurov314@gmail.com>
2018-08-07 14:53:16 -0700

    Created HtmlHelper

---
+++ ZokuChat/Helpers/HtmlHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Html;
+using ZokuChat.Data;
+using ZokuChat.Models;
+using ZokuChat.Services;
+
+namespace ZokuChat.Helpers
+{
+    public class HtmlHelper
+    {
+		public static HtmlString GetContactRequestButton(
+			Context context,
+			IContactRequestService contactRequestService,
+			User user,
+			string cssClasses = "")
+		{
+			if (contactRequestService.HasActiveContactRequest(context.CurrentUser, user))
+			{
+				return new HtmlString($"<button class='btn btn-success {cssClasses}' onclick='' disabled>Request Sent</button>");
+			}
+			else
+			{
+				return new HtmlString($"<button class='btn btn-primary {cssClasses}' onclick=''>Send Request</button>");
+			}
+		}
+
+		public static HtmlString GetContactRemoveButton(Contact contact, string cssClasses = "")
+		{
+			return new HtmlString($"<button class='btn btn-danger {cssClasses}' onclick=''>Remove Contact</button>");
+		}
+
+		public static HtmlString GetUserBlockButton(User user, string cssClasses = "")
+		{
+			return new HtmlString($"<button class='btn btn-danger {cssClasses}' onclick=''>Block User</button>");
+		}
+    }
+}
