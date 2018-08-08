70a1dffd517158605e540cac1e0e8e8f97bb5714
chiposur <osurov314@gmail.com>
2018-08-08 14:35:23 -0700

    Fixed datetime extension methods

---
+++ ZokuChat/Extensions/DateExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ZokuChat.Extensions
+{
+    public static class DateExtensions
+    {
+		const string TIME_REGEX = "h:mm tt";
+		const string DATE_REGEX = "d/M/yyyy";
+
+		public static string ToZokuChatTimeString(this DateTime dateTime)
+		{
+			return dateTime.ToString(TIME_REGEX, CultureInfo.InvariantCulture);
+		}
+
+		public static string ToZokuChatDateString(this DateTime dateTime)
+		{
+			return dateTime.ToString(DATE_REGEX, CultureInfo.InvariantCulture);
+		}
+
+		public static string ToZokuChatDateTimeString(this DateTime dateTime)
+		{
+			return $"{dateTime.ToString(DATE_REGEX, CultureInfo.InvariantCulture)} at {dateTime.ToString(TIME_REGEX, CultureInfo.InvariantCulture)}";
+		}
+	}
+}
