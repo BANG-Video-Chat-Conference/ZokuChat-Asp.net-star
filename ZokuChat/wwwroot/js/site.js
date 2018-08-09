9fd82a23d66d276414ff398a9be6f1af6a4cd347
chiposur <osurov314@gmail.com>
2018-08-09 10:11:17 -0700

    Refactored site.js

--- ZokuChat/wwwroot/js/site.js
+++ ZokuChat/wwwroot/js/site.js
@@ -1,92 +1,94 @@
-﻿window.ZokuChat = window.ZokuChat || {};
+﻿window.ZokuChat = window.ZokuChat || {
 
-window.ZokuChat.AcceptRequestButtonClick = function (clickedButton, id) {
-	// Change button to indicate request accepted
-	clickedButton.removeClass('btn-primary')
-		.addClass('btn-success')
-		.html('Request Accepted')
-		.attr('disabled', 'disabled');
+	AcceptRequestButtonClick: function (clickedButton, id) {
+		// Change button to indicate request accepted
+		clickedButton.removeClass('btn-primary')
+			.addClass('btn-success')
+			.html('Request Accepted')
+			.attr('disabled', 'disabled');
 
-	// Send ajax request to ContactRequestController
-	window.ZokuChat.SendAjaxRequest(`/ContactRequests/Accept?requestId=${id}`);
-};
+		// Send ajax request to ContactRequestController
+		window.ZokuChat.SendAjaxRequest(`/ContactRequests/Accept?requestId=${id}`);
+	},
 
-window.ZokuChat.CancelRequestButtonClick = function (clickedButton, id) {
-	// Change button to indicate request cancelled
-	clickedButton.html('Request Declined')
-		.attr('disabled', 'disabled');
+	CancelRequestButtonClick: function (clickedButton, id) {
+		// Change button to indicate request cancelled
+		clickedButton.html('Request Declined')
+			.attr('disabled', 'disabled');
 
-	// Send ajax request to ContactRequestController
-	window.ZokuChat.SendAjaxRequest(`/ContactRequests/Cancel?requestId=${id}`);
-};
+		// Send ajax request to ContactRequestController
+		window.ZokuChat.SendAjaxRequest(`/ContactRequests/Cancel?requestId=${id}`);
+	},
 
-window.ZokuChat.SendRequestButtonClick = function (clickedButton, id) {
-	// Change button to indicate request sent
-	clickedButton.removeClass('btn-primary')
-		.addClass('btn-success')
-		.html('Request Sent')
-		.attr('disabled', 'disabled');
+	SendRequestButtonClick: function (clickedButton, id) {
+		// Change button to indicate request sent
+		clickedButton.removeClass('btn-primary')
+			.addClass('btn-success')
+			.html('Request Sent')
+			.attr('disabled', 'disabled');
 
-	// Send ajax request to ContactRequestController
-	window.ZokuChat.SendAjaxRequest(`/ContactRequests/Create?requestedUID=${id}`);
-};
+		// Send ajax request to ContactRequestController
+		window.ZokuChat.SendAjaxRequest(`/ContactRequests/Create?requestedUID=${id}`);
+	},
 
-window.ZokuChat.BlockUserButtonClick = function (clickedButton, id) {
-	// Change button to indicate user blocked
-	clickedButton.html('User Blocked')
-		.attr('disabled', 'disabled');
+	BlockUserButtonClick: function (clickedButton, id) {
+		// Change button to indicate user blocked
+		clickedButton.html('User Blocked')
+			.attr('disabled', 'disabled');
 
-	// Send ajax request to BlockedUserController
-	window.ZokuChat.SendAjaxRequest(`/BlockedUsers/Block?blockedUID=${id}`);
-};
+		// Send ajax request to BlockedUserController
+		window.ZokuChat.SendAjaxRequest(`/BlockedUsers/Block?blockedUID=${id}`);
+	},
 
-window.ZokuChat.UnblockUserButtonClick = function (clickedButton, id) {
-	// Change button to indicate user unblocked
-	clickedButton.removeClass('btn-primary')
-		.addClass('btn-success')
-		.html('User Unblocked')
-		.attr('disabled', 'disabled');
+	UnblockUserButtonClick: function (clickedButton, id) {
+		// Change button to indicate user unblocked
+		clickedButton.removeClass('btn-primary')
+			.addClass('btn-success')
+			.html('User Unblocked')
+			.attr('disabled', 'disabled');
 
-	// Send ajax request to BlockedUserController
-	window.ZokuChat.SendAjaxRequest(`/BlockedUsers/Unblock?blockedUID=${id}`);
-};
+		// Send ajax request to BlockedUserController
+		window.ZokuChat.SendAjaxRequest(`/BlockedUsers/Unblock?blockedUID=${id}`);
+	},
 
-window.ZokuChat.RemoveContactButtonClick = function (clickedButton, id) {
-	// Change button to indicate contact removed
-	clickedButton.removeClass('btn-primary')
-		.addClass('btn-success')
-		.html('Contact Removed')
-		.attr('disabled', 'disabled');
+	RemoveContactButtonClick: function (clickedButton, id) {
+		// Change button to indicate contact removed
+		clickedButton.removeClass('btn-primary')
+			.addClass('btn-success')
+			.html('Contact Removed')
+			.attr('disabled', 'disabled');
 
-	// Send ajax request to ContactController
-	window.ZokuChat.SendAjaxRequest(`/Contacts/Remove?contactId=${id}`);
-};
+		// Send ajax request to ContactController
+		window.ZokuChat.SendAjaxRequest(`/Contacts/Remove?contactId=${id}`);
+	},
 
-window.ZokuChat.SendAjaxRequest = function (url, successCallback, errorCallback) {
-	$.ajax({
-		url: url,
-		dataType: 'json'
-	}).done(function (data) {
-		if (data.IsSuccessful) {
-			if (successCallback) {
-				successCallback();
+	SendAjaxRequest: function (url, successCallback, errorCallback) {
+		$.ajax({
+			url: url,
+			dataType: 'json'
+		}).done(function (data) {
+			if (data.IsSuccessful) {
+				if (successCallback) {
+					successCallback();
+				}
+			} else {
+				if (errorCallback) {
+					errorCallback();
+				}
 			}
-		} else {
-			if (errorCallback) {
-				errorCallback();
-			}
-		}
-	});
-};
+		});
+	},
 
-window.ZokuChat.GetLocalDateStr = function (date) {
-	return `${1 + date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
-};
+	GetLocalDateStr: function (date) {
+		return `${1 + date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+	},
 
-window.ZokuChat.GetLocalTimeStr = function (date) {
-	return `${date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
-};
+	GetLocalTimeStr: function (date) {
+		return `${date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
+	},
 
-window.ZokuChat.GetLocalDateTimeStr = function (date) {
-	return `${window.ZokuChat.GetLocalDateStr(date)} at ${window.ZokuChat.GetLocalTimeStr(date)}`;
+	GetLocalDateTimeStr: function (date) {
+		return `${window.ZokuChat.GetLocalDateStr(date)} at ${window.ZokuChat.GetLocalTimeStr(date)}`;
+	}
+
 };
