d4bdaeb722b6edb4192e7715fb0098327654a7da
chiposur <osurov314@gmail.com>
2018-08-12 16:14:08 -0700

    Sweeping button and user card style refactoring

--- ZokuChat/Helpers/HtmlHelper.cs
+++ ZokuChat/Helpers/HtmlHelper.cs
@@ -20,33 +20,33 @@ namespace ZokuChat.Helpers
 				int requestId = contactRequestService.GetContactRequestsFromUserToUser(user, context.CurrentUser).Where(r => r.IsActive()).Select(c => c.Id).First();
 
 				return new HtmlString(
-					$"<button class='btn btn-success request-{requestId} {cssClasses} margin-right' onclick=\"window.ZokuChat.AcceptRequestButtonClick($(this), '{requestId}');\">Accept Request</button>" +
-					$"<button class='btn btn-danger request-{requestId} {cssClasses}' onclick=\"window.ZokuChat.CancelRequestButtonClick($(this), '{requestId}');\">Decline Request</button>"
+					$"<button class='btn btn-outline-success request-{requestId} {cssClasses} margin-right' onclick=\"window.ZokuChat.AcceptRequestButtonClick($(this), '{requestId}');\">Accept Request</button>" +
+					$"<button class='btn btn-outline-danger request-{requestId} {cssClasses}' onclick=\"window.ZokuChat.CancelRequestButtonClick($(this), '{requestId}');\">Decline Request</button>"
 				);
 			}
 			else if (contactRequestService.HasActiveContactRequest(context.CurrentUser, user))
 			{
-				return new HtmlString($"<button class='btn btn-success {cssClasses}' disabled>Request Sent</button>");
+				return new HtmlString($"<button class='btn btn-outline-success {cssClasses}' disabled>Request Sent</button>");
 			}
 			else
 			{
-				return new HtmlString($"<button class='btn btn-primary {cssClasses}' onclick=\"window.ZokuChat.SendRequestButtonClick($(this), '{user.Id}');\">Send Request</button>");
+				return new HtmlString($"<button class='btn btn-outline-primary {cssClasses}' onclick=\"window.ZokuChat.SendRequestButtonClick($(this), '{user.Id}');\">Send Request</button>");
 			}
 		}
 
 		public static HtmlString GetContactRemoveButton(Contact contact, string cssClasses = "")
 		{
-			return new HtmlString($"<button class='btn btn-danger {cssClasses}' onclick=\"window.ZokuChat.RemoveContactButtonClick($(this), '{contact.Id}')\">Remove Contact</button>");
+			return new HtmlString($"<button class='btn btn-outline-danger {cssClasses}' onclick=\"window.ZokuChat.RemoveContactButtonClick($(this), '{contact.Id}')\">Remove Contact</button>");
 		}
 
 		public static HtmlString GetBlockUserButton(User user, string cssClasses = "")
 		{
-			return new HtmlString($"<button class='btn btn-danger {cssClasses}' onclick=\"window.ZokuChat.BlockUserButtonClick($(this), '{user.Id}');\">Block User</button>");
+			return new HtmlString($"<button class='btn btn-outline-danger {cssClasses}' onclick=\"window.ZokuChat.BlockUserButtonClick($(this), '{user.Id}');\">Block User</button>");
 		}
 
 		public static HtmlString GetUnblockUserButton(User user, string cssClasses = "")
 		{
-			return new HtmlString($"<button class='btn btn-primary {cssClasses}' onclick=\"window.ZokuChat.UnblockUserButtonClick($(this), '{user.Id}');\">Unblock User</button>");
+			return new HtmlString($"<button class='btn btn-outline-primary {cssClasses}' onclick=\"window.ZokuChat.UnblockUserButtonClick($(this), '{user.Id}');\">Unblock User</button>");
 		}
 	}
 }
