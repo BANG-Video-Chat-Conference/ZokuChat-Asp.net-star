8bf6c539157292f0b9ecdd55dc7951dae1ba49fc
chiposur <osurov314@gmail.com>
2018-08-14 16:28:52 -0700

    More work on chat page ui

--- ZokuChat/wwwroot/js/chatHub.js
+++ ZokuChat/wwwroot/js/chatHub.js
@@ -1,6 +1,5 @@
 ﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-
-
+var roomId;
 
 connection.start().catch(function (err) {
 	return console.error(err.toString());
