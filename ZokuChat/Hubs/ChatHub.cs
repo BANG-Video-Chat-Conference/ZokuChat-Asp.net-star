b0662f578a2a87e896258579cd28a78ead72c282
chiposur <osurov314@gmail.com>
2018-08-14 18:30:32 -0700

    Added join group functionality to chat hub and style tweaks

--- ZokuChat/Hubs/ChatHub.cs
+++ ZokuChat/Hubs/ChatHub.cs
@@ -20,6 +20,31 @@ namespace ZokuChat.Hubs
 			_roomService = roomService;
 		}
 
+		public async Task JoinRoom(int roomId)
+		{
+			if (roomId <= 0)
+			{
+				await ReturnError("Could not join room", "You must specify a room.");
+				return;
+			}
+
+			Room room = null;
+			await Task.Run(() => room = _roomService.GetRoom(roomId));
+
+			if (room == null || !RoomPermissionHelper.CanViewRoom(_context.CurrentUser, room))
+			{
+				await ReturnError("Could not join room", "You do not have permission, the room may have been deleted.");
+				return;
+			}
+
+			await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString());
+		}
+
+		public async Task LeaveRoom(int roomId)
+		{
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId.ToString());
+		}
+
 		public async Task DeleteMessage(int roomId, int messageId)
 		{
 			// Validation
@@ -44,7 +69,7 @@ namespace ZokuChat.Hubs
 			await Task.Run(() => _roomService.DeleteMessage(_context.CurrentUser, message));
 
 			// Notify all in group
-			await Clients.All.SendAsync("DeleteMessage", _context.CurrentUser.UserName, messageId);
+			await Clients.OthersInGroup(roomId.ToString()).SendAsync("DeleteMessage", _context.CurrentUser.UserName, messageId);
 		}
 
 		public async Task SendMessage(int roomId, string text)
@@ -82,7 +107,7 @@ namespace ZokuChat.Hubs
 			await Task.Run(() => _roomService.AddMessage(_context.CurrentUser, room, text));
 
 			// Notify all in group
-			await Clients.All.SendAsync("ReceiveMessage", _context.CurrentUser.UserName, text);
+			await Clients.OthersInGroup(roomId.ToString()).SendAsync("ReceiveMessage", _context.CurrentUser.UserName, text);
 		}
 
 		public async Task ReturnError(string errorCaption, string errorMessage)
--- ZokuChat/wwwroot/js/chatHub.js
+++ ZokuChat/wwwroot/js/chatHub.js
@@ -1,6 +1,21 @@
 ﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 var roomId;
+var creatorId;
+var currentUserId;
+
+connection.on("ReceiveMessage", function (userName, text) {
+	
+});
+
+connection.on("ReceiveError", function (caption, message) {
+
+});
 
 connection.start().catch(function (err) {
 	return console.error(err.toString());
+}).then(function (value) {
+	// Join the chat room group
+	connection.invoke("JoinRoom", roomId).catch(function (err) {
+		return console.error(err.toString());
+	});
 });
